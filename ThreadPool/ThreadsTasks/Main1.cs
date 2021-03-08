dde44bbccc23a522e1352705e67fc60caa61cc8f
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-03-08 21:15:39 +0200

    Add files via upload

---
+++ ThreadPool/ThreadsTasks/Main1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsTasks
+{
+    class Main_
+    {
+        public static void Main1()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread t1 = new Thread(() =>
+            {
+                // 0.05
+                Thread t2 = new Thread(() =>
+                {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("I am background thread ...");
+                });
+                Thread.Sleep(500);
+                Console.WriteLine("I am background thread ...");
+                t2.Join(); // t1 is waiting for t2
+            });
+            t1.IsBackground = false; // default
+            t1.IsBackground = true;
+            t1.Start(); // 0.001
+            //Thread.Sleep(200);
+            //t1.Abort
+
+            //t1.Suspend
+            //t1.Resume
+
+            // 0.002
+            t1.Join(); // main thread will not wait for t1 
+        }
+
+    }
+}
---
+++ ThreadPool/ThreadsTasks/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsTasks
+{
+    class Program
+    {
+        static int[] counter = new int[1];
+        static void MyWork()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int j = i * 5;
+            }
+            counter[0]++;
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // ignite - block
+
+            List<Thread> threads = new List<Thread>();
+            int max = 1200;
+
+            // penalty time
+
+            for (int i = 0; i < max; i++)
+            {
+                //Thread t = new Thread(MyWork);
+                //t.Start();
+                //threads.Add(t);
+                ThreadPool.QueueUserWorkItem((o) => { MyWork(); });
+            }
+
+            while (counter[0] < 1000)
+            {
+                Thread.Sleep(1);
+            }
+
+            Console.WriteLine($"It took {sw.ElapsedMilliseconds}");
+            sw.Stop();
+        }
+    }
+}
